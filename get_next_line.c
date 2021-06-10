ab03015a22580a7d1f92b799b3099e72d9e4a02a
Tiffany MANOLIS <tiffany.mano@gmail.com>
2021-06-10 18:27:29 +0200

    Baby step

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tmanolis <tmanolis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 11:17:55 by tmanolis          #+#    #+#             */
-/*   Updated: 2021/06/08 13:55:57 by tmanolis         ###   ########.fr       */
+/*   Updated: 2021/06/10 18:25:43 by tmanolis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,45 @@
 
 size_t  ft_strlen(const char *str);
 size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+char *ft_strjoin(char const *s1, char const *s2);
 char    *ft_strdup(const char *s1);
+
+char	*copy_until_EOF(char *buf, char *stock)
+{
+	int i;
+	char *line = NULL;
+
+// il faut peut gerer la size de stock qui fluctue avec un stock[j] ?
+	
+	i = 0;
+	*stock = '\0';
+	while (ft_strchr(stock, '\n') == 0)
+	{
+		stock = ft_strjoin(stock, buf);
+
+	}
+	while (stock[i] != '\n')
+	{
+		// il faut malloc line avant donc strlen de stock jusqu'a '\n' pour la taille
+		// du malloc de line.
+		line[i] = stock[i];
+		i++;
+	}
+	return (line);
+}
 
 int get_next_line(int fd, char **line)
 {
-    char    buf[BUFFER_SIZE + 1];
-    ssize_t ret;
+    char    	buf[BUFFER_SIZE + 1];
+    ssize_t 	ret;
+	static char	*stock;
 
     ret = read(fd, buf, BUFFER_SIZE);
     while (ret > 0)
     {
         buf[ret] = '\0';
-        *line = ft_strdup(buf);
+        *line = copy_until_EOF(buf, stock);
         ret = read(fd, buf, BUFFER_SIZE);
     }
 	return (1);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: tmanolis <tmanolis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 17:25:08 by tmanolis          #+#    #+#             */
-/*   Updated: 2021/06/08 13:56:00 by tmanolis         ###   ########.fr       */
+/*   Updated: 2021/06/10 17:49:09 by tmanolis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,46 @@ size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize)
         }
         dst[i] = '\0';
         return (ft_strlen(src));
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0' && *s == c)
+		return ((char *)s);
+	else
+		return (0);
+}
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*new_str;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	new_str = (char	*)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+  	if (!new_str)
+		return (NULL);
+	while (s1[i])
+	{
+		new_str[j++] = s1[i++];
+	}
+	i = 0;
+	while (s2[i])
+	{
+		new_str[j++] = s2[i++];
+	}
+	new_str[j] = '\0';
+	return (new_str);
 }
 
 char    *ft_strdup(const char *s1)
