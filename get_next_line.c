5e81d43c9b39ada0acf1f3917c2bb6c1d1af2b05
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-06-14 15:07:55 +0200

    Commentaire

--- get_next_line.c
+++ get_next_line.c
@@ -26,22 +26,30 @@ char	*copy_until_EOF(char *buf, char *stock)
 	int i;
 	char *line = NULL;
 
-// il faut peut gerer la size de stock qui fluctue avec un stock[j] ?
+	// il faut peut gerer la size de stock qui fluctue avec un stock[j] ?
 	
 	i = 0;
-	*stock = '\0';
-	while (ft_strchr(stock, '\n') == 0)
-	{
-		stock = ft_strjoin(stock, buf);
 
-	}
 	while (stock[i] != '\n')
-	{
+	{	
+		// stock = "hello\n prout"
 		// il faut malloc line avant donc strlen de stock jusqu'a '\n' pour la taille
 		// du malloc de line.
-		line[i] = stock[i];
 		i++;
 	}
+	// i = le nombre de charactere jusqua \n
+	// adresse de &i - &stock = taille
+	
+	// on malloc line a la taille de i + 1 
+	// str_n_copy() (source: stock, des: line, n: i)
+	str_n_copy(line, stock, i) // on part du principe pour l exemple que copy malloc
+	
+
+	// stock = "hello\nhihi"
+	// transformation pif paf
+	// stock = "hihi"
+
+	// renvoie l address de line
 	return (line);
 }
 
@@ -51,13 +59,38 @@ int get_next_line(int fd, char **line)
     ssize_t 	ret;
 	static char	*stock;
 
-    ret = read(fd, buf, BUFFER_SIZE);
-    while (ret > 0)
-    {
-        buf[ret] = '\0';
-        *line = copy_until_EOF(buf, stock);
-        ret = read(fd, buf, BUFFER_SIZE);
+	// faut malloc buf ""
+	// "hello" & BUFFER_SIZE = 2 => buf = "he" 
+	// "hello" & BUFFER_SIZE = 7 => buf = "hello\0\0" 
+	// "hello" & BUFFER_SIZE = 1000 => buf = "hello\0\0...\0" est ce que c est free?
+	
+
+    ret = read(fd, buf, BUFFER_SIZE); 
+    while (ret > 0 && ft_strchr(buf, '\n') == 0)
+    {	
+		
+		// tant qu'on ne trouve pas de \n on ajoute buf a la suite de stock
+		stock = ft_strjoin(stock, buf); // y a une libft join qui malloc
+	
+		ret = read(fd, buf, BUFFER_SIZE);
+		// est ce utile buf[ret] ?
+		buf[ret] = '\0'; // verifier que ca permet de free jusqu'au bout 
     }
+
+	
+	// verifier qu'il y a bien rien dans buf et si il y a alors l'ajouter a stock
+	// buffer = "\nhihi"
+	// stock = "haha"
+	// faut mettre buffer dans stock
+
+	// stock = "haha\nhihi"
+	if (strlen(stock) > 0) {
+		*line = copy_until_EOF(stock); // get the following line
+	}
+	else {
+		return (0)
+	}
+	
 	return (1);
 }
 
