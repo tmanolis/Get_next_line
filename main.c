59b22baa1ecf860ffd1547065e7f1e0f951be7fc
Tiffany MANOLIS <tiffany.mano@gmail.com>
2021-07-06 18:04:16 +0200

    test bonus

--- main.c
+++ main.c
@@ -3,17 +3,24 @@
 
 int main(void)
 {
-	int		fd;
+	int		fd[2];
 	char	*line;
 
 	line = NULL;
-	fd = open("test.txt", O_RDONLY);
-	while (get_next_line(fd, &line))
-	{
-		printf(">>> line : |%s| <<<\n", line);
-		free(line);
-	}
+	fd[0] = open("test_bonus.txt", O_RDONLY);
+	fd[1] = open("test_bonus2.txt", O_RDONLY);
+	// while (get_next_line(fd, &line))
+	// {
+	// 	printf(">>> line : |%s| <<<\n", line);
+	// 	free(line);
+	// }
+	get_next_line(fd[0], &line);
 	printf(">>> line : |%s| <<<\n", line);
-	close (fd);
+	free(line);
+	get_next_line(fd[1], &line);
+	printf(">>> line : |%s| <<<\n", line);
+	free(line);
+	close (fd[0]);
+	close (fd[1]);
 	return (0);
 }
